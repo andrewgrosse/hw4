e8e8c90c6d933cfd9e2f520392da340a3e11eae4
Andrew Grosse <andrew.grosse@kellogg.northwestern.edu>
2025-03-11 23:04:02 +0000

    login and logout logic

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,25 @@
+require 'digest' #hashing passwords manually
+
 class SessionsController < ApplicationController
   def new
   end
 
   def create
+    user = User.find_by(email: params[:email])
+
+    #check if user exists and password is correct
+    if user && user["password"] ==Digest::SHA256.hexigest(params[:password])
+      session[:user_id] = user.id
+      redirect_to "/places"
+    else
+      flash["notice"] = "Invalid email or password"
+      redirect_to "/login"
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to "/login"
   end
 end
   
