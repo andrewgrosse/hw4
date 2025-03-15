5a7b05d44678ee0e2d619d0e2ef9d209a689349d
Andrew Grosse <andrew.grosse@kellogg.northwestern.edu>
2025-03-15 19:30:17 +0000

    Design

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_03_12_001744) do
+ActiveRecord::Schema[7.1].define(version: 2025_03_15_192418) do
   create_table "entries", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -26,6 +26,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_12_001744) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.string "address"
   end
 
   create_table "users", force: :cascade do |t|
