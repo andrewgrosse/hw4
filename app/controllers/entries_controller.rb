b7c3de95e20a52d5bef80dcae532d3ecde431daa
Andrew Grosse <andrew.grosse@kellogg.northwestern.edu>
2025-03-15 18:54:32 +0000

    entries work

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,13 +1,20 @@
 class EntriesController < ApplicationController
   def new
     @entry = Entry.new
-    @place = Place.find(params[:place_id])
+    @place = Place.find(params[:place_id])  # ✅ Ensure the correct place is assigned
   end
 
   def create
     @entry = Entry.new(entry_params)
     @entry.user_id = session[:user_id]
-    @entry.place_id = params[:place_id]
+
+    # ✅ Ensure place_id is set correctly
+    if params[:entry][:place_id].present?
+      @entry.place_id = params[:entry][:place_id]
+    else
+      flash[:alert] = "Error: Entry must be associated with a place."
+      render :new and return
+    end
 
     if @entry.save
       redirect_to place_path(@entry.place_id), notice: "Entry added successfully!"
@@ -20,6 +27,6 @@ class EntriesController < ApplicationController
   private
 
   def entry_params
-    params.require(:entry).permit(:title, :description, :occurred_on)
+    params.require(:entry).permit(:title, :description, :occurred_on, :place_id)
   end
 end
